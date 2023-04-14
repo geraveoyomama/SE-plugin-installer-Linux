471a7611f685d3595a7919d738ddbbd030764aa8
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2023-04-14 15:27:33 +0200

    Update SE-PL-Linux.sh

--- SE-PL-Linux.sh
+++ SE-PL-Linux.sh
@@ -7,7 +7,7 @@ else
 fi
 echo "Using, $PWD"
 sleep 1
-wget -q https://gist.githubusercontent.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/raw/SpaceEngineersLauncher.py -O SpaceEngineersLauncher.py
+wget https://gist.githubusercontent.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/raw/SpaceEngineersLauncher.py -O SpaceEngineersLauncher.py
 echo "Opekope's script downloaded"
 sleep 1
 wget https://github.com/sepluginloader/SpaceEngineersLauncher/releases/latest/download/SpaceEngineersLauncher.exe -O SpaceEngineersLauncher.exe
