feed5c850a935b72460f05155edb6ea0b8d1065b
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2023-04-14 15:40:47 +0200

    fixed up the speed and added warning

--- SE-PL-Linux.sh
+++ SE-PL-Linux.sh
@@ -6,10 +6,13 @@ else
   cd $gamepath/Bin64
 fi
 echo "Using, $PWD"
-sleep 1
 wget https://gist.githubusercontent.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/raw/SpaceEngineersLauncher.py -O SpaceEngineersLauncher.py
-echo "Opekope's script downloaded"
-sleep 1
+echo "Opekope's script downloaded!"
 wget https://github.com/sepluginloader/SpaceEngineersLauncher/releases/latest/download/SpaceEngineersLauncher.exe -O SpaceEngineersLauncher.exe
+echo "Avaness' SpaceEngineersLauncher downloaded!"
+echo "Setting execute permissions for downloaded files..."
 chmod +x SpaceEngineersLauncher.exe SpaceEngineersLauncher.py
-echo "Add './SpaceEngineersLauncher.py %command%' to your game's launch options."
+echo "Permissions set!"
+echo ""
+echo "DONT FORGET!! Add './SpaceEngineersLauncher.py %command%' to your game's launch options."
+sleep 2
