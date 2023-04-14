3cc3ade4e9b8c12519e617dd8594bd7013ca0f02
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2023-04-14 15:23:54 +0200

    permalinked Opekope's script

--- SE-PL-Linux.sh
+++ SE-PL-Linux.sh
@@ -7,7 +7,7 @@ else
 fi
 echo "Using, $PWD"
 sleep 1
-wget -qO- https://gist.github.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/archive/a54ad222faf4483a3f63098bea8240bf19350697.zip | busybox unzip -j -
+wget -q https://gist.githubusercontent.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/raw/SpaceEngineersLauncher.py -O SpaceEngineersLauncher.py
 echo "Opekope's script downloaded"
 sleep 1
 wget https://github.com/sepluginloader/SpaceEngineersLauncher/releases/latest/download/SpaceEngineersLauncher.exe -O SpaceEngineersLauncher.exe
