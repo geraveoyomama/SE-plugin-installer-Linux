d3fb25b37c77d3b6e6d94e380358b88966819a39
geraveoyomama <77015630+geraveoyomama@users.noreply.github.com>
2024-09-30 14:31:35 +0200

    Update SE-PL-Linux.sh

--- SE-PL-Linux.sh
+++ SE-PL-Linux.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+
+autopath=$(find ~ -type d | grep 'SpaceEngineers/Bin64' |awk 'NR==1{print $1}')
+gamepath=${autopath%%/Bin64*}/Bin64
+
+
 if [[ $1 ]]; then
   gamepath=$1
 else
-  read -p "where is your Space Engineers game folder?: " gamepath
+  read -p "Is $gamepath your Space Engineers Bin64 folder?"
 fi
-cd $gamepath/Bin64
+
+cd $gamepath
 echo "Using, $PWD"
 wget https://gist.githubusercontent.com/opekope2/e02db7e526dadff0813a6ea2aebf820b/raw/SpaceEngineersLauncher.py -O SpaceEngineersLauncher.py
 echo "Opekope's script downloaded!"
 wget https://github.com/sepluginloader/SpaceEngineersLauncher/releases/latest/download/SpaceEngineersLauncher.exe -O SpaceEngineersLauncher.exe
 echo "Avaness' SpaceEngineersLauncher downloaded!"
 echo "Setting execute permissions for downloaded files..."
-chmod +x SpaceEngineersLauncher.exe SpaceEngineersLauncher.py
+chmod ug+rx SpaceEngineersLauncher.exe SpaceEngineersLauncher.py
 echo "Permissions set!"
 echo ""
 echo "DONT FORGET!! Add './SpaceEngineersLauncher.py %command%' to your game's launch options."
